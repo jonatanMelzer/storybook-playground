98a5009dd556fc9bae00a216580bc8e61c0dc68d
jonatanMelzer <jonatan.melzer@ifolor.com>
2023-10-31 16:25:46 +0100

    add cromatic

--- src/stories/color-button.stories.ts
+++ src/stories/color-button.stories.ts
@@ -1,6 +1,7 @@
 import { Meta, StoryObj, componentWrapperDecorator } from '@storybook/angular';
 import { ColorButtonComponent } from 'src/color/color-button/color-button.component';
-
+import { within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
 
 // More on how to set up stories at: https://storybook.js.org/docs/angular/writing-stories/introduction
 const meta: Meta<ColorButtonComponent> = {
@@ -17,9 +18,17 @@ type Story = StoryObj<ColorButtonComponent>;
 
 
 // More on writing stories with args: https://storybook.js.org/docs/angular/writing-stories/args
-export const ColourButton: Story = {
+export const FilledColourButton: Story = {
   args: {
     color: '#95C533',
-    isSelected: false,
+    isSelected: true,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const element = canvas.getByRole('checkmark');
+
+    await expect(element).toBeInTheDocument();
   }
 };
+
+
